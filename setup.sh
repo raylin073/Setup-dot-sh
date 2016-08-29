0c33c3d379cf69d51e8eef11a54c21987c161d49
AJua <ljredracess@gmail.com>
2016-08-29 14:28:25 +0800

    add more packages

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-curl https://debgen.simplylinux.ch/txt/stretch/sources_062ecaa39c5b5f8d3167faa886542114fd27eb31.txt | sudo tee /etc/apt/sources.list
+wget https://raw.githubusercontent.com/AJua/Setup-dot-sh/master/sources.list -O /tmp/sources.list 
+cp /tmp/sources.list /etc/apt/sources.list
 
 apt-get update
-apt-get install git python-pip python-dev
+apt-get install git python-pip python-dev vim-gtk pcmanx-gtk2
 
 git clone https://github.com/ajua/dotvim ~/.vim
 ln -s ~/.vimrc ~/.vim/_vimrc
