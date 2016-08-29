f886958fe268a71d722a88e43020f9f2ce70904c
AJua <ljredracess@gmail.com>
2016-08-29 13:58:25 +0800

    add ignore pattern

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 apt-get update
-apt-get install 
+apt-get install git python-pip python-dev
+
+git clone https://github.com/ajua/dotvim ~/.vim
+cd ~/.vim/bundle
+git submodule update --init --recursive
+
