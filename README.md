727591195046030b3100b1c61a4ff23548f6a7da
AJua <ljredracess@gmail.com>
2016-08-29 14:49:12 +0800

    add readme and setup script

---
+++ README.md
@@ -0,0 +1,3 @@
+``` sh
+wget https://raw.githubusercontent.com/AJua/Setup-dot-sh/master/setup.sh -O /tmp/setup.sh && bash /tmp/setup.sh
+```
--- setup.sh
+++ setup.sh
@@ -4,10 +4,17 @@ wget https://raw.githubusercontent.com/AJua/Setup-dot-sh/master/sources.list -O 
 cp /tmp/sources.list /etc/apt/sources.list
 
 apt-get update
-apt-get install git python-pip python-dev vim-gtk pcmanx-gtk2
+apt-get install -y curl git python-pip python-dev vim-gtk nvidia-cuda-toolkit pcmanx-gtk2
 
+# nodejs 4.x
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# vim config
 git clone https://github.com/ajua/dotvim ~/.vim
 ln -s ~/.vimrc ~/.vim/_vimrc
 cd ~/.vim/bundle
 git submodule update --init --recursive
+
+
 
